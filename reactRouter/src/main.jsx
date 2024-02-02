bf13b851bc3c31affb033eda6e61679cea3ed46f
Shiv <shivankamwar@gmail.com>
2024-02-02 15:06:27 +0530

    first commit

--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -8,7 +8,7 @@ import About from './components/About/About.jsx'
 import Home from './components/Home/Home.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import User from './components/User/User.jsx'
-
+import Github, { githubInfoLoader } from './components/Github/Github.jsx'
 // const router=createBrowserRouter([
 //   {
 //     path: '/',
@@ -36,6 +36,10 @@ const router=createBrowserRouter(
     <Route path='about' element={<About/>}/>
     <Route path='contact' element={<Contact/>}/>
     <Route path='user/:userid' element={<User/>}/>
+    <Route
+    loader={githubInfoLoader} 
+    path='github' 
+    element={<Github/>}/>
   </Route>)
 )
  
